2a2a31dddc37d23690e95a16763cfe37aacf0bbf
yahyamomin <yahyamomin06@gmail.com>
2023-09-15 17:25:14 +0530

    error fixed

--- public/javascript/main.js
+++
@@ -1,9 +0,0 @@
-import { name, show } from './temp';
-
-console.log(name);
-
-show();
-
-let a = document.querySelector('h1');
-
-a.innerHTML = name;
--- public/javascript/temp.js
+++
@@ -1,6 +0,0 @@
-export let name = "yahya"
-
-export function show(){
-    console.log("hello world");
-}
-
